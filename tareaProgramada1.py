922e1f3717506f9ff8011bcda6146189cba57a64
MigaleMR <alemarra98@gmail.com>
2023-09-28 14:22:01 -0600

    Tarea programada

---
+++ tareaProgramada1.py
@@ -0,0 +1,10 @@
+#####################################################################
+#Elaborado por: Alejandro Madrigal y Daniel Campos
+#Fecha de creación: 21-09-2023 Hora: 10:00pm
+#Fecha de finalización:
+#Versión: 3.11.5
+####################################################################
+import pandas as pd
+df=pd.read_excel("D:\Estudios de Ale\Compu\Intro y taller\intro-taller2S2023\TallerProgra\Tarea Programada\paises.xlsx")
+
+
