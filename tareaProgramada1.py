e4d8c06d506dbad9ee7d10c93772bd6f98734032
MigaleMR <alemarra98@gmail.com>
2023-10-02 15:22:27 -0600

    Informacion paises, territorio, continente, poblacion
    Se crearon las funciones para seleccionar el continente, territorio de los paises y poblacion

--- tareaProgramada1.py
+++ tareaProgramada1.py
@@ -81,13 +81,23 @@ infoPaises = crearPaisesLista(paises)
 # call the function to create the XML file
 listaPaises(infoPaises)
 
-def generar_html_paises(info_paises):
+def htmlContinente(infoPaises):
+    # Preguntar al usuario por el continente de interés
+    continenteElegido = input("Ingrese el nombre del continente del cual desea obtener información (Europe, North America, Asia, Antartica, Africa, Oceania ): ")##############CAMBIAR##############
+
+    # Filtrar los países por continente
+    filtrarPaises = [pais for pais in infoPaises if pais[5][0] == continenteElegido]
+
+    if not filtrarPaises:
+        print(f"No se encontraron países en el continente {continenteElegido}.")
+        return
+
     # Crear el documento HTML
     html = BeautifulSoup("<html><head><title>Información de Países</title></head><body></body></html>", "html.parser")
 
     # Agregar encabezado con estilo
     encabezado = html.new_tag("h1", style="color: blue; text-align: center;")
-    encabezado.string = "Información de Países"
+    encabezado.string = f"Información de Países en el Continente {continenteElegido}"
     html.body.append(encabezado)
 
     # Agregar tabla de países con estilo
@@ -97,30 +107,133 @@ def generar_html_paises(info_paises):
     # Agregar encabezados de tabla con estilo
     encabezados = html.new_tag("tr")
     tabla.append(encabezados)
-    encabezados_tags = ["Nombre", "Código", "Territorio", "Población", "Capital"]
-    for encabezado_tag in encabezados_tags:
+    tags = ["Nombre", "Código", "Población", "Capital", "Área"]
+    for tags in  tags:
         th = html.new_tag("th", style="background-color: lightgray; padding: 10px; text-align: left;")
-        th.string = encabezado_tag
+        th.string =  tags
         encabezados.append(th)
 
     # Contador para alternar colores de filas
-    fila_color = 0
+    color = 0
 
     # Agregar filas de datos de países con estilo alternado
-    for pais in info_paises:
-        fila = html.new_tag("tr", style=f"background-color: {'#CCE6FF' if fila_color % 2 == 0 else '#99C2FF'};")
-        fila_color += 1
+    for pais in filtrarPaises:
+        fila = html.new_tag("tr", style=f"background-color: {'#CCE6FF' if color % 2 == 0 else '#99C2FF'};")
+        color += 1
         tabla.append(fila)
-        nombre, codigos, poblacion, capital, area = pais[:5]
-        for dato in [nombre, codigos[0], poblacion, capital, area[9]]:
+        nombre, codigos, poblacion, capital, area = pais[0], pais[1], pais[3], pais[4], pais[7]
+        for dato in [nombre, codigos[0], poblacion, capital, area]:
             td = html.new_tag("td", style="padding: 5px;")
             td.string = str(dato)
             fila.append(td)
 
     # Guardar el archivo HTML
-    with open("informacion_paises.html", "w", encoding="utf-8") as file:
+    with open(f"informacion_{continenteElegido}.html", "w", encoding="utf-8") as file:
         file.write(str(html))
-
 # Llama a la función para generar el HTML basado en la información de países
 infoPaises = crearPaisesLista(paises)  # Reemplaza datos_paises con tu lista de países
-generar_html_paises(infoPaises)
+htmlContinente(infoPaises)
+
+def poblacionMayorMenor(infoPaises):
+    # Implementar ordenación personalizada por población de mayor a menor
+    poblacionMayorMenor= []
+
+    while infoPaises:
+        maximo = infoPaises[0]
+        for pais in infoPaises:
+            if pais[3] > maximo[3]:
+                maximo = pais
+        poblacionMayorMenor.append(maximo)
+        infoPaises.remove(maximo)
+
+    # Crear el documento HTML
+    html = BeautifulSoup("<html><head><title>Información de Países por Población</title></head><body></body></html>", "html.parser")
+
+    # Agregar encabezado con estilo
+    encabezado = html.new_tag("h1", style="color: blue; text-align: center;")
+    encabezado.string = "Información de Países por Población"
+    html.body.append(encabezado)
+
+    # Agregar tabla de países con estilo
+    tabla = html.new_tag("table", style="width: 100%; border-collapse: collapse;")
+    html.body.append(tabla)
+
+    # Agregar encabezados de tabla con estilo
+    encabezados = html.new_tag("tr")
+    tabla.append(encabezados)
+    encabezados_tags = ["Población", "isoAlpha3", "Nombre del país", "Área en metros cuadrados", "Nombre del continente"]
+    for encabezado_tag in encabezados_tags:
+        th = html.new_tag("th", style="background-color: lightgray; padding: 10px; text-align: left;")
+        th.string = encabezado_tag
+        encabezados.append(th)
+    fila_color=0
+    # Agregar filas de datos de países ordenados por población
+    for pais in poblacionMayorMenor:
+        fila = html.new_tag("tr", style=f"background-color: {'#CCE6FF' if fila_color % 2 == 0 else '#99C2FF'};")
+        fila_color += 1
+        tabla.append(fila)
+        poblacion, isoAlpha3, nombre_pais, area, continente = pais[3], pais[1][3], pais[0], pais[7], pais[5][0]
+        for dato in [poblacion, isoAlpha3, nombre_pais, area, continente]:
+            td = html.new_tag("td", style="padding: 5px;")
+            td.string = str(dato)
+            fila.append(td)
+
+    # Guardar el archivo HTML
+    with open("informacion_por_poblacion.html", "w", encoding="utf-8") as file:
+        file.write(str(html))
+
+# Llama a la función para mostrar la información de países ordenada por población
+poblacionMayorMenor(infoPaises)
+# Función para mostrar información de países ordenada por área en metros cuadrados sin sorted
+infoPaises = crearPaisesLista(paises)
+def territorioPaises(infoPaises):
+    # Implementar ordenación personalizada por población de mayor a menor
+    info_paises_ordenados = []
+
+    while infoPaises:
+        max_pais = infoPaises[0]
+        for pais in infoPaises:
+            if pais[6] > max_pais[6]:
+                max_pais = pais
+        info_paises_ordenados.append(max_pais)
+        infoPaises.remove(max_pais)
+
+    # Crear el documento HTML
+    html = BeautifulSoup("<html><head><title>Información de Países por Población</title></head><body></body></html>", "html.parser")
+
+    # Agregar encabezado con estilo
+    encabezado = html.new_tag("h1", style="color: blue; text-align: center;")
+    encabezado.string = "Información de Países por Población"
+    html.body.append(encabezado)
+
+    # Agregar tabla de países con estilo
+    tabla = html.new_tag("table", style="width: 100%; border-collapse: collapse;")
+    html.body.append(tabla)
+
+    # Agregar encabezados de tabla con estilo
+    encabezados = html.new_tag("tr")
+    tabla.append(encabezados)
+    encabezados_tags = ["Área en metros cuadrados", "fipsCode", "Nombre del país", "Nombre del continente"]
+    for encabezado_tag in encabezados_tags:
+        th = html.new_tag("th", style="background-color: lightgray; padding: 10px; text-align: left;")
+        th.string = encabezado_tag
+        encabezados.append(th)
+    fila_color=0
+    # Agregar filas de datos de países ordenados por población
+    for pais in info_paises_ordenados:
+        fila = html.new_tag("tr", style=f"background-color: {'#CCE6FF' if fila_color % 2 == 0 else '#99C2FF'};")
+        fila_color += 1
+        tabla.append(fila)
+        area, fipsCode, nombrePais, continente = pais[6], pais[1][1], pais[0], pais[5][1]
+        for dato in [area, fipsCode, nombrePais, continente]:
+            td = html.new_tag("td", style="padding: 5px;")
+            td.string = str(dato)
+            fila.append(td)
+
+    # Guardar el archivo HTML
+    with open("informacion_por_poblacion.html", "w", encoding="utf-8") as file:
+        file.write(str(html))
+
+# Llama a la función para mostrar la información de países ordenada por área
+territorioPaises(infoPaises)
+
